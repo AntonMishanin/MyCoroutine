5a930bab572c2f212aa86e5eed3c464129bd1c34
Anton Mishanin <antonmishanin@mail.ru>
2021-09-28 21:03:10 +0700

    add several coroutine features

---
+++ app/src/main/java/com/nnnnn/coroutine/MyActivity.kt
@@ -0,0 +1,102 @@
+package com.nnnnn.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlin.system.measureTimeMillis
+
+class MyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("EE", "1 currentThread = ${Thread.currentThread()}")
+        lifecycleScope.launch(Dispatchers.Main) {
+            Log.d("EE", "2 currentThread = ${Thread.currentThread()}")
+            myLaunch()
+        }
+
+        val myScope = CoroutineScope(Job() + Dispatchers.IO)
+        val job = myScope.launch {
+
+        }
+
+        //Не привязан к моему скопу
+        GlobalScope.launch {
+
+        }
+        // Log.d("EE", "DO")
+        //Блокирует основной поток
+        runBlocking {
+            // Log.d("EE", "START")
+            async(Dispatchers.IO) {
+                val startTime = System.currentTimeMillis()
+                while (System.currentTimeMillis() - startTime < 1_000) {
+
+                }
+            }
+        }
+
+        //Это не связано с корутинами, но с помощью этой фичи можно посмотреть,
+        //за сколько времени выполнился код внутри
+        measureTimeMillis {
+
+        }
+
+        //Обработка исключений
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+
+        }
+        lifecycleScope.launch(context = exceptionHandler) {
+
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            Log.d("EE", "START")
+            val result = fetchContent()
+            Log.d("EE", "RESULT = $result")
+        }
+        Log.d("EE", "POSLE")
+
+        val myFlow: Flow<Int> = flow {
+            emit(4)
+            emit(5)
+
+        }.map {
+            it
+        }.filter {
+            true
+        }.collect {
+
+        }
+    }
+
+    private suspend fun fetchContent(): Int {
+        return coroutineScope {
+            val result1 = async {
+                val startTime = System.currentTimeMillis()
+                while (System.currentTimeMillis() - startTime < 5_000) {
+
+                }
+                return@async 5
+            }
+            val result2 = async {
+                val startTime = System.currentTimeMillis()
+                while (System.currentTimeMillis() - startTime < 7_000) {
+
+                }
+                return@async 3
+            }
+            return@coroutineScope result1.await() + result2.await()
+        }
+    }
+
+    private suspend fun myLaunch() {
+        // Log.d("EE", "3 currentThread = ${Thread.currentThread()}")
+        delay(1_000)
+        //Log.d("EE", "4 currentThread = ${Thread.currentThread()}")
+    }
+}
